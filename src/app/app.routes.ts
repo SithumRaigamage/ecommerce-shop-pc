f1ef532e70352913e829eed005c098cd08cf20ee
Sithum Raigamage <sraig2002@gmail.com>
2025-02-22 14:31:07 +0530

    basic setup of support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import {SupportComponent} from './support/support.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+  },
+  {
+    path: 'support',
+    component: SupportComponent,
   }
 ];
---
+++ src/app/support/support.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-support',
+  imports: [ReactiveFormsModule],
+  templateUrl: './support.component.html',
+  styleUrl: './support.component.css'
+})
+export class SupportComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+contactForm: any;
+
+}
