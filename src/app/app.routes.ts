467c72d62e1847edaaf2ef7e2e8c4027d30dabfe
Sithum Raigamage <sraig2002@gmail.com>
2025-02-23 22:46:03 +0530

    product-overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {SupportComponent} from './support/support.component';
 import {ProductGridComponent} from './product-grid/product-grid.component';
+import { ProductOverviewComponent } from './product-overview/product-overview.component';
 
 export const routes: Routes = [
   {
@@ -15,4 +16,8 @@ export const routes: Routes = [
   { path: 'product-grid',
     component: ProductGridComponent
   },
+  {
+    path: 'product-overview/:id',
+    component: ProductOverviewComponent
+  }
 ];
