7b36473a4f5a80b74d29d0444aec14321c9deefb
Sithum Raigamage <sraig2002@gmail.com>
2025-02-23 14:31:26 +0530

    mainlayout

--- src/app/mainlayout/mainlayout.component.ts
+++ src/app/mainlayout/mainlayout.component.ts
@@ -3,6 +3,7 @@ import { MainNavbarComponent } from '../main-navbar/main-navbar.component';
 import { MainContentComponent } from '../main-content/main-content.component';
 import { FooterComponent } from '../footer/footer.component';
 import { SideNavComponent } from '../side-nav/side-nav.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mainlayout',
@@ -12,4 +13,10 @@ import { SideNavComponent } from '../side-nav/side-nav.component';
 })
 export class MainlayoutComponent {
 
+  constructor(private router: Router) {}
+
+  onCategorySelected(category: string): void {
+    console.log('category selected: ', category);
+    this.router.navigate(['/product-grid'], { queryParams: { category } });
+  }
 }
